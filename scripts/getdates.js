5643731152b97c09422e7dcf3848b96b0e28c2d9
James Michelson <jejedi110@gmail.com>
2025-11-01 10:45:34 -0700

    Fixed javascript error

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,9 +2,14 @@ const year = document.querySelector("#current-year");
 
 const today = new Date();
 
-year.innerHTML = `<span class="current-year">${today.getFullYear()}</span>`;
+if (year) {
+    year.innerHTML = `<span class="current-year">${today.getFullYear()}</span>`;
+}
+
 
 
 const populated = document.querySelector("#last-populated");
 
-populated.innerHTML = `Last modified: <span class="last-populated">${document.lastModified}</span>`;document.getElementById("lastModified").innerHTML = document.lastModified;
+if (populated) {
+    populated.innerHTML = `Last modified: <span class="last-populated">${document.lastModified}</span>`;
+}
