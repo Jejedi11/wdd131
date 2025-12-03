e3ab0d608849063312e2c15d62e24b507927c1de
James <jejedi110@pm.me>
2025-12-03 16:11:55 -0700

    local-storage

--- week05/scripts/bom.js
+++ week05/scripts/bom.js
@@ -3,23 +3,49 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 
-button.addEventListener('click', function() {
-    if (input.value.trim() !== '') {
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        list.append(li);
-        input.focus();
-        input.value = '';
-    }
-    else {
-        input.focus();
-    }
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+  displayList(chapter);
 });
 
-deleteButton.addEventListener('click', function() {
-    list.removeChild(li);
+
+button.addEventListener('click', () => {
+  if (input.value.trim() != '') {
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
     input.focus();
-})
+  }
+});
+
+function displayList(item) {
+  let li = document.createElement('li');
+  let deleteButton = document.createElement('button');
+  li.textContent = item;
+  deleteButton.textContent = '❌';
+  deleteButton.classList.add('delete');
+  li.append(deleteButton);
+  list.append(li);
+  deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    deleteChapter(li.textContent);
+    input.focus();
+  })
+}
+
+function setChapterList() {
+  localStorage.setItem('favBomList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('favBomList'));
+}
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter((item) => item != chapter);
+  setChapterList();
+}
+
