b85fb5f96f170070c63fdb079f2e05237cdf1d9f
James <jejedi110@pm.me>
2025-12-04 20:05:26 -0700

    Review project

---
+++ scripts/form.js
@@ -0,0 +1,35 @@
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+for (let i = 0; i < products.length; i++) {
+  const productMenu = document.querySelector("#product-name");
+  productItem = document.createElement("option");
+  productItem.innerHTML = `${products[i].name}`;
+  productItem.id = `${products[i].id}`;
+  productMenu.appendChild(productItem);
+}
