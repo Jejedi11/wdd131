0dcadc4db332351356e1bd91b0cc952f162321d6
James Michelson <jejedi110@gmail.com>
2025-12-04 21:10:52 -0700

    Counter messages

--- scripts/review.js
+++ scripts/review.js
@@ -1,1 +1,12 @@
 
+if (parseInt(localStorage.getItem('submissions')) == null) {
+  localStorage.setItem('submissions', "1");
+}
+else {
+  localStorage.setItem(parseInt(localStorage.getItem('submissions')) + 1);
+}
+
+const counterMessage = document.createElement('p');
+counterMessage.innerHTML = `You have submitted ${getItem('submissions')} reviews.`;
+document.querySelector('body').appendChild(counterMessage);
+
